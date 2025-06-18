652085b8e305e68119eb937935b2df1dcdd0b762
Jeppe <jepjorg10@gmail.com>
2025-06-18 14:59:12 +0200

    kleine fix

--- src/game/observer/GameStatusObserver.java
+++ src/game/observer/GameStatusObserver.java
@@ -4,7 +4,7 @@ import game.Speler;
 
 public class GameStatusObserver implements SpelerObserver {
     public void update(Speler speler) {
-        System.out.println("📍 Je bent nu in kamer: " + speler.getPositie());
+        System.out.println("------------------------------\n📍 Je bent nu in kamer: " + speler.getPositie());
 
         if (!speler.getMonsters().isEmpty()) {
             System.out.println("🧟‍♂️ Monster: " + speler.getMonsters().get(speler.getMonsters().size() - 1).getNaam());
@@ -12,6 +12,6 @@ public class GameStatusObserver implements SpelerObserver {
             System.out.println("🧟‍♂️ Monster: Geen actieve monsters.");
         }
 
-        System.out.println("🚪 Je hebt toegang tot kamer: " + (speler.getPositie() + 1));
+        System.out.println("🚪 Je hebt toegang tot kamer: " + (speler.getPositie() + 1)+ "\n------------------------------");
     }
 }
