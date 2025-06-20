f123824df5bdce2abf7bf11900de5daa78d21a58
Mick <24065803@student.hhs.nl>
2025-06-20 13:15:57 +0200

    Update MockTest.java

--- src/test/Mick/MockTest.java
+++ src/test/Mick/MockTest.java
@@ -1,29 +1,9 @@
 package test.Mick;
-package game.observer;
 
-import game.Speler;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MockTest {
 
-    @Test
-    void testSpelerWaarschuwtMockObserver() {
-        // Arrange: maak een speler en een mock observer
-        Speler speler = new Speler();
-        MockObserver mockObserver = new MockObserver();
-        speler.addObserver(mockObserver);
-
-        // Act: roep notifyObservers aan
-        speler.notifyObservers();
-
-        // Assert: controleer of de mock observer is opgeroepen
-        assertTrue(mockObserver.isOpgeroepen, "De observer moet zijn opgeroepen");
-    }
 }
-
-
 
 
    
