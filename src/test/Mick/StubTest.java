1ff0cd43cc1e0c3f3f1b465af4eb16f78b4d7c5e
Mick <24065803@student.hhs.nl>
2025-06-20 12:33:58 +0200

    Update StubTest.java

--- src/test/Mick/StubTest.java
+++ src/test/Mick/StubTest.java
@@ -1,4 +1,30 @@
 package test.Mick;
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StubTest {
+
+    @Test
+    void testGetNaam() {
+        Monster monster = new Monster("Draak", "Een vuurspuwend monster");
+        assertEquals("Draak", monster.getNaam());
+    }
+
+    @Test
+    void testToString() {
+        Monster monster = new Monster("Trol", "Een groot en sterk monster");
+        assertEquals("Trol", monster.toString());
+    }
+
+    // Optioneel: geen assert, maar laat zien hoe toonMonster werkt
+    @Test
+    void testToonMonster() {
+        Monster monster = new Monster("Zombie", "Langzaam maar eng");
+        monster.toonMonster();  // Dit print alleen naar de console
+    }
 }
+
+
