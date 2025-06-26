7aea418aa62ff0d2fd62df8c8db823f2322ed72f
kenrickkock <23075511@student.hhs.nl>
2025-06-26 10:52:50 +0200

    Kamer en Kamerfinale codesmells verwijderd

--- src/game/kamer/Kamer.java
+++ src/game/kamer/Kamer.java
@@ -4,29 +4,49 @@ import game.Speler;
 import game.vraag.Vraag;
 import game.voorwerp.Voorwerp;
 
-
-
 public class Kamer {
     private final String naam;
-    protected final Vraag vraag;
-    private final Voorwerp voorwerp;
+    private final Vraag vraag;
+         private final Voorwerp voorwerp;
 
     public Kamer(String naam, Vraag vraag, Voorwerp voorwerp) {
+
+
         this.naam = naam;
+
         this.vraag = vraag;
         this.voorwerp = voorwerp;
     }
 
-    public boolean voerUit(Speler speler) {
-        System.out.println("Je bent in de kamer: " + naam);
+      public boolean voerUit(Speler speler) {
+        System.out.println(getBeschrijving());
         return vraag.stelVraag(speler);
     }
 
-    public String getNaam() {
+    public String getBeschrijving() {
+        return "Je bent in de kamer: " + naam;
+    }
+        public String getNaam() {
         return naam;
     }
 
+
     public Voorwerp getVoorwerp() {
         return voorwerp;
+    }
+
+    public Vraag getVraag() {
+        return vraag;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Kamer{" +
+                "naam='" + naam + '\'' +
+                ", voorwerp=" + (voorwerp != null ? voorwerp.getClass().getSimpleName() : "geen") +
+                '}';
     }
 }
--- src/game/kamer/KamerFinale.java
+++ src/game/kamer/KamerFinale.java
@@ -1,15 +1,10 @@
 package game.kamer;
 
 import game.Speler;
-import game.vraag.InvulVraag;
 import game.vraag.MeerkeuzeVraag;
-import game.vraag.Vraag;
 import game.voorwerp.StandaardVoorwerp;
 
-
 public class KamerFinale extends Kamer {
-    //private Vraag vraag;
-
 
     public KamerFinale() {
         super(
@@ -21,18 +16,16 @@ public class KamerFinale extends Kamer {
                                 "B) Transparantie, Inspectie en Aanpassing",
                                 "C) Technische Integratie Aanpak"
                         },
-
-                        "b"
+                        "B" // Consistent met de antwoordopties
                 ),
                 new StandaardVoorwerp("Scimitar", "🗡️ Je verslaat het monster met de Ottomaanse Scimitar!")
         );
-
     }
 
     @Override
     public boolean voerUit(Speler speler) {
         System.out.println("Je bent in de kamer: " + getNaam());
-        boolean juist = vraag.stelVraag(speler);
+        boolean juist = getVraag().stelVraag(speler);
         if (juist) {
             System.out.println("✅ Geweldig! Je hebt het Scrum Escape Game gewonnen!");
         } else {
