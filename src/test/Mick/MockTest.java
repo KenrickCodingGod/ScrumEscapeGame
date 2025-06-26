c0d12103f0e4ef35011862b068d27c5e38cfe5e2
Mick <24065803@student.hhs.nl>
2025-06-26 17:58:31 +0200

    Update MockTest.java

--- src/test/Mick/MockTest.java
+++ src/test/Mick/MockTest.java
@@ -1,4 +1,25 @@
 package test.Mick;
 
-public class MockTest {
+import game.Speler;
+import game.observer.SpelerObserver;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class MockTestObserverCall {
+
+    @Test
+    void testUpdateWordtAangeroepenOpObserver() {
+      
+        Speler speler = new Speler();
+        SpelerObserver observerMock = mock(SpelerObserver.class);
+
+        speler.voegObserverToe(observerMock);
+
+        
+        speler.veranderPositie(3);
+
+       
+        verify(observerMock, times(1)).update(speler);
+    }
 }
